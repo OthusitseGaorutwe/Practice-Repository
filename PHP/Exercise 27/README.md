678c761cf5a5177e743ca9000b79f4d141ea16e0
Othusitse Donald Gaorutwe <otgaorutwe@gmail.com>
2026-03-06 21:21:39 +0200

    PHP: Exercise 27 completed

---
+++ PHP/Exercise 27/README.md
@@ -0,0 +1,3 @@
+27. Print PHP Credits
+
+Write a PHP script to print out all the credits for PHP.
---
+++ PHP/Exercise 27/index.php
@@ -0,0 +1,4 @@
+<?php
+    // Call the phpcredits function with the specified credits constant
+    phpcredits(CREDITS_ALL - CREDITS_FULLPAGE);
+?>
