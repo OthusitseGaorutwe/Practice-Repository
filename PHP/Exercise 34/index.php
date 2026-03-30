f338931bd5df85e94956a1011142e3192482a11c
Othusitse Donald Gaorutwe <otgaorutwe@gmail.com>
2026-03-30 22:41:18 +0200

    PHP: Exercise 34 revised and completed

--- PHP/Exercise 34/index.php
+++ PHP/Exercise 34/index.php
@@ -3,6 +3,7 @@
 
     $bi = null;
 
+    // loop through the number and devide by 2 each time up until it is 0
     while($number > 0){
         if($number % 2 == 0){
             $bi = 0 . $bi;
@@ -19,6 +20,7 @@
 
     $results = null;
 
+    // compare values at each positions
     if($first_pos >= 0 && $second_post >= 0){
         if($first_pos < $length && $second_post < $length){
             if($bi[$first_pos] === $bi[$second_post]){
@@ -33,6 +35,7 @@
         $results = 'The postion cannot be negative';
     }
 
+    // results
     echo "Binary: " . $bi;
     echo '<br>';
     echo "Postions: " . $first_pos . " and " . $second_post;
